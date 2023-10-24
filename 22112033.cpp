22ec82a8b3ce9a3870334c7f6bbcebcdd21747c8
iamgoeldhruv <goeldhruv9876@gmail.com>
2023-10-25 03:45:57 +0530

    taken all input from file

--- 22112033.cpp
+++ 22112033.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<map>
 using namespace std;
 int main(int argc, char* argv[]) {
     string pageTableFile = argv[1];
@@ -9,12 +10,47 @@ int main(int argc, char* argv[]) {
     int tlbSize = atoi(argv[4]);
     ifstream pgtfile(pageTableFile);
     ifstream tlbfile(tlbAccessesFile);
-    vector<long long int>vpn;
-    vector<long long int>pfn;
+    map<int,int>vpn;
+    map<int,int>pfn;
+    vector<int>tlbaccess;
+   
+    string vpnvalues;
+    int cvpn=0;
+    int i=1;
+    while(pgtfile>>vpnvalues){
+        cvpn++;
+        if(cvpn>4 and cvpn%2!=0){
+
+            vpn[i]=stoi(vpnvalues);
+           
+
+        }
+        if(cvpn>4 and cvpn%2==0){
+
+            pfn[i]=stoi(vpnvalues);
+            i++;
+           
+
+        }
+       
+    }
+    int tlbaccessvalues;
+    int z=0;
+    while(tlbfile>>tlbaccessvalues){
+        z++;
+        if(z>1){
+            tlbaccess.push_back(tlbaccessvalues);
+
+        }
+
+    }
+     cout<<tlbaccess[1]<<endl;
+    
     
 
 
-    
+ 
+
   
     return 0;
 
